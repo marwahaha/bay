9ba6e09fec3daa1dd8bc9edc675a40368d195afc
Kunal Marwaha <marwahaha@berkeley.edu>
2022-02-09 04:06:07 -0600

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -11,27 +11,4 @@
    });
  });
 
-/**
- * Wechat widget
- */
-function moveWidget(event) {
-  var w = $("#wechat-widget");
-
-  w.css({
-    left: event.pageX - 25,
-    top: event.pageY - w.height() - 60
-  });
-}
-
-$("a#wechat-link").on("mouseenter", function(event) {
-  $("#wechat-widget").css({ display: "block" });
-  moveWidget(event);
-});
-
-$("a#wechat-link").on("mousemove", function(event) {
-  moveWidget(event);
-});
-
-$("a#wechat-link").on("mouseleave", function(event) {
-  $("#wechat-widget").css({ display: "none" });
-});
+$("#homeimgcontainer")[0].innerHTML += "<div>서울, January 2019.</div>"
