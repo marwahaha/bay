be164b1cf6468b56a796a259abf6147baf307a30
Kunal Marwaha <marwahaha@berkeley.edu>
2022-02-09 04:28:46 -0600

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -12,3 +12,15 @@
  });
 
 $("#homeimgcontainer")[0].innerHTML += "<div>서울, January 2019.</div>"
+
+ $("script[type='math/tex']").replaceWith(
+    function(){
+      var tex = $(this).text();
+      return katex.renderToString(tex, {displayMode: false});
+  });
+  
+  $("script[type='math/tex; mode=display']").replaceWith(
+    function(){
+      var tex = $(this).text();
+      return katex.renderToString(tex.replace(/%.*/g, ''), {displayMode: true});
+  });
